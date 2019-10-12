60d274380c68ccd09e5f652dbe4d66513bd11e21
svt <sean.tommasi@gmail.com>
2019-10-12 02:21:31 -0400

    Get email from command-line argument

--- diffsend.sh
+++ diffsend.sh
@@ -2,11 +2,11 @@
 
 path_file="$1"
 regex="$2"
+email="$3"
 
 base_file="${path_file##*/}"
 temp_file="/tmp/${base_file}.tmp"
 diff_file="/tmp/${base_file}.diff"
-email="sean.tommasi@gmail.com"
 
 if [ -e "$temp_file" ]; then
     diff <(grep "$regex" "$temp_file") <(grep "$regex" "$path_file") > "$diff_file"
