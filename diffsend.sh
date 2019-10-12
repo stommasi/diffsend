13afe46ff8e8a0483c48837829b027d2d3fa3ac9
svt <sean.tommasi@gmail.com>
2019-10-12 02:39:30 -0400

    Document the program's general purpose and functionality

--- diffsend.sh
+++ diffsend.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# diffsend - compare file to last time script was run and email diff report
+#
+# This script takes a filename, a regular expression, and an email address as
+# arguments. If the file has never been checked before, the script makes a copy
+# of it in /tmp and exits. On the next run, it checks to see if lines matching
+# the regular expression have been added or removed from the file in the
+# meantime, i.e. it checks for updates in regards to a particular matching
+# string. If there is a difference, the diff report is emailed to the specified
+# address. This is useful for monitoring log files.
+
 path_file="$1"
 regex="$2"
 email="$3"
