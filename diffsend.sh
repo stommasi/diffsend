c6d97a16df3992d4edef001e759e7b09ed7f03c9
svt <sean.tommasi@gmail.com>
2019-10-12 02:20:45 -0400

    Put second argument into  variable

--- diffsend.sh
+++ diffsend.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 path_file="$1"
+regex="$2"
+
 base_file="${path_file##*/}"
 temp_file="/tmp/${base_file}.tmp"
 diff_file="/tmp/${base_file}.diff"
 email="sean.tommasi@gmail.com"
 
 if [ -e "$temp_file" ]; then
-    diff <(grep "$2" "$temp_file") <(grep "$2" "$path_file") > "$diff_file"
+    diff <(grep "$regex" "$temp_file") <(grep "$regex" "$path_file") > "$diff_file"
     if [ $? -ne 0 ]; then
         mutt -s "${base_file} update" "$email" < "$diff_file"
     fi
