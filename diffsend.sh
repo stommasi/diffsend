49e96c6883df5e291016c0732a178a294b6b78f3
svt <sean.tommasi@gmail.com>
2019-10-12 02:07:10 -0400

    Restructure conditional branching

--- diffsend.sh
+++ diffsend.sh
@@ -8,13 +8,9 @@ email="sean.tommasi@gmail.com"
 
 if [ -e "$temp_file" ]; then
     diff <(grep "$2" "$temp_file") <(grep "$2" "$path_file") > "$diff_file"
-else
-    cp "$path_file" "$temp_file"
-    exit 0
-fi
-
-if [ $? -ne 0 ]; then
-    mutt -s "${base_file} update" $email < "$diff_file"
+    if [ $? -ne 0 ]; then
+        mutt -s "${base_file} update" "$email" < "$diff_file"
+    fi
 fi
 
 cp "$path_file" "$temp_file"
